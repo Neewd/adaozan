ffbdeb7884e9f7b455ac42af3fcf03596c2ad2e1
Neewd <clement.bacouelle@gmail.com>
2024-01-08 21:55:58 +0100

    Some fixes

--- components/LogoCloud.tsx
+++ components/LogoCloud.tsx
@@ -1,13 +1,35 @@
 import Image from "next/image";
+import Link from "next/link";
 
-const logos: string[] = [
-	"/img/partenaires/region-bretagne.png",
-	"/img/partenaires/technopole.png",
-	"/img/partenaires/grdf.png",
-	"/img/partenaires/live-for-good.png",
-	"/img/partenaires/banque-postale.png",
-	"/img/partenaires/makesense.png",
-	"/img/partenaires/fondation-de-france.png",
+const logos: { imageUrl: string; linkUrl: string }[] = [
+	{
+		imageUrl: "/img/partenaires/region-bretagne.png",
+		linkUrl: "https://aides.bretagne.bzh/aides/#/crbr/",
+	},
+	{
+		imageUrl: "/img/partenaires/technopole.png",
+		linkUrl: "https://7technopoles-bretagne.bzh/",
+	},
+	{
+		imageUrl: "/img/partenaires/grdf.png",
+		linkUrl: "https://www.grdf.fr/",
+	},
+	{
+		imageUrl: "/img/partenaires/live-for-good.png",
+		linkUrl: "https://www.live-for-good.org/",
+	},
+	{
+		imageUrl: "/img/partenaires/banque-postale.png",
+		linkUrl: "https://www.labanquepostale.fr/",
+	},
+	{
+		imageUrl: "/img/partenaires/makesense.png",
+		linkUrl: "https://makesense.org/",
+	},
+	{
+		imageUrl: "/img/partenaires/fondation-de-france.png",
+		linkUrl: "https://www.fondationdefrance.org/fr/",
+	},
 ];
 
 export const LogoCloud = () => {
@@ -18,15 +40,17 @@ export const LogoCloud = () => {
 					return (
 						<div
 							className="px-12 flex-1 py-4 flex-1bg-cream-100  z-20 flex items-center justify-center"
-							key={logo}
+							key={logo.imageUrl}
 						>
-							<Image
-								className="flex items-center justify-center"
-								src={logo}
-								width={200}
-								height={200}
-								alt=""
-							/>
+							<Link href={logo.linkUrl} target="_blank">
+								<Image
+									className="flex items-center justify-center"
+									src={logo.imageUrl}
+									width={200}
+									height={200}
+									alt=""
+								/>
+							</Link>
 						</div>
 					);
 				})}
--- components/Products.tsx
+++ components/Products.tsx
@@ -1,30 +1,39 @@
-import { ProductHero } from './ProductHero';
-import { products } from './produits.data';
+import { ProductHero } from "./ProductHero";
+import { products } from "./produits.data";
 
-export default async function Products() {
-  return (
-    <>
-      <div className="flex z-10 relative">
-        {products.map((product) => {
-          return (
-            <ProductHero
-              key={product.id}
-              imageUrl={product.imageUrl}
-              title={product.title}
-              subtitle={product.subtitle}
-              objectPosition={product.objectPosition}
-              slug={product.slug}
-              id={product.id}
-              carousel={product.carousel}
-              content={product.content}
-              subContent={product.subContent}
-              details={product.details}
-              price={product.price}
-              orderButton={product.orderButton}
-            />
-          );
-        })}
-      </div>
-    </>
-  );
+export default async function Products({
+	withDescription = false,
+}: {
+	withDescription: boolean;
+}) {
+	return (
+		<>
+			<div className="flex z-10 relative">
+				{products.map((product) => {
+					return (
+						<ProductHero
+							key={product.id}
+							imageUrl={product.imageUrl}
+							title={product.title}
+							subtitle={product.subtitle}
+							objectPosition={product.objectPosition}
+							slug={product.slug}
+							id={product.id}
+							carousel={product.carousel}
+							content={product.content}
+							subContent={product.subContent}
+							details={product.details}
+							price={product.price}
+							orderButton={product.orderButton}
+							description={
+								withDescription
+									? product.description
+									: undefined
+							}
+						/>
+					);
+				})}
+			</div>
+		</>
+	);
 }
